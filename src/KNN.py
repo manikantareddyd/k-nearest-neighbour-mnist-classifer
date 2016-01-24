34b7ea43d858a4c95b41587b46c643db7069dd9c
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-01-24 07:41:11 +0530

    Build Freeze - This is done for now. Note to Self: Run time is Really Large

---
+++ src/KNN.py
@@ -0,0 +1,35 @@
+from mnist import MNIST
+import numpy as np
+from sklearn.neighbors import  KNeighborsClassifier
+
+mn  = MNIST('./data')
+train_raw = mn.load_training()
+test_raw = mn.load_testing()
+
+train_labels = []
+train_data = []
+test_labels = []
+test_data=[]
+for i in range(0,60000):
+	train_labels.append(train_raw[1][i])
+	train_data.append(train_raw[0][i])
+for i in range(0,10000):
+	test_labels.append(test_raw[1][i])
+	test_data.append(test_raw[0][i])
+
+print "Holololol"
+del train_raw
+del mn
+del test_raw
+
+nbrsModel = KNeighborsClassifier(\
+	n_neighbors=5,\
+	weights = 'distance',\
+	algorithm = 'auto',\
+	metric = 'minkowski',\
+	p=2,\
+	n_jobs = -1)
+print "POP"
+nbrsModel.fit(train_data,train_labels)
+print "POOP"
+print(nbrsModel.score(test_data,test_labels))
